c85ad5ef9f6cc25308c0b82794ba881173edf2d7
Liam Adams <liam1540@googlemail.com>
2023-12-05 17:29:14 -0500

    Add initial test (won't build yet because we don't have MPI_COMM_WORLD yet).

---
+++ Tests/swift_mpiTests/swift_mpiTests.swift
@@ -0,0 +1,42 @@
+import XCTest
+@testable import swift_mpi
+
+final class swift_mpiTests: XCTestCase {
+    func test_mpi_send_recv() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+
+
+        swift_mpi_init(CommandLine.argc, CommandLine.unsafeArgv)
+
+        var comm =  MPI_COMM_WORLD
+        var size: Int32 = 0
+        var rank: Int32 = 0
+        swift_mpi_comm_size(comm, &size)
+        swift_mpi_comm_rank(comm, &rank)
+
+        let count = 4
+        var sendbuf = [Double](repeating: Double(rank + 1), count: count)
+        var recvbuf = [Double](repeating: Double(rank + 1), count: count)
+
+        if size > 1 {
+            if rank == 0 {
+                swift_mpi_send( sendbuf, 1, 0, comm)
+                swift_mpi_recv(&recvbuf, 1, 0, comm)
+                XCTAssertEqual(sendbuf, [Double](repeating: 1, count: count), "sendbuf has not changed.")
+                XCTAssertEqual(recvbuf, [Double](repeating: 2, count: count), "recvbuf equal to sendbuf on rank 1.")
+            }
+            if rank == 1 {
+                swift_mpi_recv(&recvbuf, 0, 0, comm)
+                swift_mpi_send( sendbuf, 0, 0, comm)
+                XCTAssertEqual(sendbuf, [Double](repeating: 2, count: count), "sendbuf has not changed.")
+                XCTAssertEqual(recvbuf, [Double](repeating: 1, count: count), "recvbuf equal to sendbuf on rank 0.")
+            }
+        }
+
+        swift_mpi_finalize()
+    }
+}
