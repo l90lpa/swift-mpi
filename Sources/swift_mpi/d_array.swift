47c689f2b63a4253f5cf278699280ee133dab5da
Liam Adams <liam1540@googlemail.com>
2024-01-05 02:23:07 +0000

    Add array utilities.

---
+++ Sources/swift_mpi/d_array.swift
@@ -0,0 +1,25 @@
+import _Differentiation
+
+// See: https://www.tensorflow.org/swift/tutorials/Swift_autodiff_sharp_edges#array_subscript_sets
+extension Array where Element: Differentiable {
+    @differentiable(where Element: Differentiable)
+    mutating func updated(at index: Int, with newValue: Element) {
+        self[index] = newValue
+    }
+
+    @derivative(of: updated)
+    mutating func vjpUpdated(at index: Int, with newValue: Element)
+      -> (value: Void, pullback: (inout TangentVector) -> (Element.TangentVector))
+    {
+        self.updated(at: index, with: newValue)
+        return ((), { v in
+            let dElement = v[index]
+            v.base[index] = .zero
+            return dElement
+        })
+    }
+}
+
+func zerosLike<T: Numeric>(_ array: [T]) -> [T] {
+    return Array(repeating: 0, count: array.count)
+}
